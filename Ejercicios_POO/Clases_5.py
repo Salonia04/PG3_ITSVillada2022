aafcb569a3018bc4acbc04da360661d87db6c491
Agus Salonia <agusalonia04@gmail.com>
2022-04-22 18:31:49 -0300

    Resolucion ej 5

--- Ejercicios_POO/Clases_5.py
+++ Ejercicios_POO/Clases_5.py
@@ -1,8 +1,25 @@
 class Persona:
-    def inicializacion(self,nombre,edad):
+    def __init__(self):
         self.nombre = input("Ingrese su nombre: ")
         self.edad = int(input("Ingrese su edad: "))
 
     def mostrar(self):
-        print("Nombre: ",self.nombre)
-        print("Edad: ",self.edad)
+        print("Nombre: ", self.nombre)
+        print("Edad: ", self.edad)
+
+class Empleado(Persona):
+    def __init__(self):
+        super().__init__()
+        self.sueldo = int(input("Ingrese su sueldo: "))
+
+    def mostrar(self):
+        super().mostrar()
+        print("Sueldo: ", self.sueldo)
+        if self.sueldo > 3000:
+            print("Paga impuestos")
+
+Persona1 = Persona()
+Persona1.mostrar()
+
+Empleado1 = Empleado()
+Empleado1.mostrar()
